6b5430ee5e3a675f22ed4effcaa02d0b3d72e897
shagnprt <shagun00019@gmail.com>
2024-08-17 10:16:43 -0300

    Add files via upload

---
+++ videowebsite/script.js
@@ -0,0 +1,7 @@
+// Example JavaScript code
+console.log("JavaScript file is connected successfully!");
+
+const animatedText = document.getElementById('animated-text');
+animatedText.addEventListener('click', () => {
+    alert("You clicked the animated text!");
+});
